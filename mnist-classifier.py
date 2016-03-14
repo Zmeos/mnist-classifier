d0fbfda632349e140140a9e83ef8bce75ba4a07b
Joakim Skarding <pinef2@gmail.com>
2016-03-14 14:37:34 +0100

    Added the download function, in order to automatically download the dataset

--- mnist-classifier.py
+++ mnist-classifier.py
@@ -1,7 +1,7 @@
 # Most of this classifier is inspired and a modified version of the lasagne tutorial: http://lasagne.readthedocs.org/en/latest/user/tutorial.html
 
 import os
-import gzip
+import sys
 import time
 
 import numpy as np
@@ -16,35 +16,57 @@ import lasagne
 
 
 # ############################# Load dataset ###############################
+# This load function is directly copyed from the Lasagne tutorial mentioned above, and almost not modified.
 def load_dataset():
-    # Read training and test sets
-    # Preprocessing by creating a cross validation set and reshaping the pixels
+    # We first define a download function, supporting both Python 2 and 3.
+    if sys.version_info[0] == 2:
+        from urllib import urlretrieve
+    else:
+        from urllib.request import urlretrieve
 
-    def read_data(filename):
+    def download(filename, source='http://yann.lecun.com/exdb/mnist/'):
+        print("Downloading %s" % filename)
+        urlretrieve(source + filename, filename)
+
+    # We then define functions for loading MNIST images and labels.
+    # For convenience, they also download the requested files if needed.
+    import gzip
+
+    def load_mnist_images(filename):
+        if not os.path.exists(filename):
+            download(filename)
+        # Read the inputs in Yann LeCun's binary format.
         with gzip.open(filename, 'rb') as f:
-                return read_csv(f, header=None)
+            data = np.frombuffer(f.read(), np.uint8, offset=16)
+        # The inputs are vectors now, we reshape them to monochrome 2D images,
+        # following the shape convention: (examples, channels, rows, columns)
+        data = data.reshape(-1, 28, 28)
+        # The inputs come as bytes, we convert them to float32 in range [0,1].
+        # (Actually to range [0, 255/256], for compatibility to the version
+        # provided at http://deeplearning.net/data/mnist/mnist.pkl.gz.)
+        return data / np.float32(256)
 
-    train_data = read_data("training_data.csv.gz")
-    test_data = read_data("test_data.csv.gz")
+    def load_mnist_labels(filename):
+        if not os.path.exists(filename):
+            download(filename)
+        # Read the labels in Yann LeCun's binary format.
+        with gzip.open(filename, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=8)
+        # The labels are vectors of integers now, that's exactly what we want.
+        return data
 
-    # Test set is already standarized in range [0-1]
+    # We can now download and read the training and test set images and labels.
+    X_train = load_mnist_images('train-images-idx3-ubyte.gz')
+    y_train = load_mnist_labels('train-labels-idx1-ubyte.gz')
+    X_test = load_mnist_images('t10k-images-idx3-ubyte.gz')
+    y_test = load_mnist_labels('t10k-labels-idx1-ubyte.gz')
 
-    #### Spawn training and cv variables ####
-    y_train = np.int32(train_data[train_data.columns[-1]].values)
-    X_train = train_data.drop(train_data.columns[-1], 1).values
-
+    # We reserve the last 10000 training examples for validation.
     X_train, X_val = X_train[:-10000], X_train[-10000:]
     y_train, y_val = y_train[:-10000], y_train[-10000:]
 
-    #### Spawn test variables ####
-    y_test = np.int32(test_data[test_data.columns[-1]].values)
-    X_test = test_data.drop(test_data.columns[-1], 1).values
-
-    #### Unroll pixels ####
-    X_train = X_train.reshape(-1,  28, 28)
-    X_test = X_test.reshape(-1, 28, 28)
-    X_val = X_val.reshape(-1, 28, 28)
-
+    # We just return all the arrays in order, as expected in main().
+    # (It doesn't matter how we do this as long as we can read them again.)
     return X_train, y_train, X_val, y_val, X_test, y_test
 
 # ############################# Displaying data ###############################
@@ -64,12 +86,14 @@ def display_images(images, labels, num_figure=1):
     assert len(images) == len(labels)
     plt.figure(num_figure)
 
+    #print len(images[1][0])
 
     # Loop will only work for even numbers
     for i in range(len(labels)):
         plt.subplot(5,2,i)
 
         # Display image
+
         plt.imshow(images[i])
         plt.axis('off')
 
@@ -137,6 +161,7 @@ def main(learning_rate=0.05,
             num_epochs=100):
 
     X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
+
 
     sel_X, sel_y = select_random_elements(X_train, y_train)
     display_images(sel_X, sel_y)
